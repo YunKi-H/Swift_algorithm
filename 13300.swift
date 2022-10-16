4a4dd88432be954bbe0026e4d4bde8d10ebe065b
maemi <dbsrl759@naver.com>
2022-10-16 22:48:48 +0900

    13300

--- 13300.swift
+++ 13300.swift
@@ -1,1 +1,18 @@
 // https://www.acmicpc.net/problem/13300
+let nk = readLine()!
+    .split(separator: " ")
+    .map{ Int(String($0))! }
+var room: [Int] = Array(repeating: 0, count: 13)
+
+for _ in 1...nk[0] {
+    let sy = readLine()!
+        .split(separator: " ")
+        .map{ Int(String($0))! }
+    room[sy[0] * 6 + sy[1]] += 1
+}
+
+var answer: Int = 0
+for i in room {
+    answer += i / nk[1] + (i % nk[1] > 0 ? 1 : 0)
+}
+print(answer)
