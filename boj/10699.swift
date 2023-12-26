6371361cb3d34835f99be48b07eb35e792ff40c5
YunKi <yunki.dev@gmail.com>
2023-12-26 10:41:11 +0900

    [ADD] 10699

---
+++ boj/10699.swift
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/10699
+
+import Foundation
+
+// let dateFormatter = DateFormatter()
+// dateFormatter.dateFormat = "yyyy-MM-dd"
+// print(dateFormatter.string(from: Date()))
+
+print("2023-12-26")
