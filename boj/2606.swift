d6b1c782f6367379281c2f98dd5fd1085bb3b31c
YunKi <yunki.dev@gmail.com>
2024-01-01 14:56:36 +0900

    [ADD] 2606 as DFS

--- boj/2606.swift
+++ boj/2606.swift
@@ -1,30 +1,56 @@
 // https://www.acmicpc.net/problem/2606
 
-import Foundation
+// import Foundation
 
-let pc = Int(readLine()!)!
-var graph: [[Int]] = Array(repeating: [], count: pc + 1)
-var visited: [Bool] = Array(repeating: false, count: pc + 1)
-let line = Int(readLine()!)!
-for _ in 0..<line {
-  let connect = readLine()!.split { $0 == " " }.map { Int(String($0))! }
-  graph[connect[0]].append(connect[1])
-  graph[connect[1]].append(connect[0])
+// /// BFS
+// let pc = Int(readLine()!)!
+// var graph: [[Int]] = Array(repeating: [], count: pc + 1)
+// var visited: [Bool] = Array(repeating: false, count: pc + 1)
+// let line = Int(readLine()!)!
+// for _ in 0..<line {
+//   let connect = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+//   graph[connect[0]].append(connect[1])
+//   graph[connect[1]].append(connect[0])
+// }
+
+// var queue = [1]
+// var index = 0
+// visited[1] = true
+
+// while queue.count > index {
+//   let currendNode = queue[index]
+//   for nextNode in graph[currendNode] {
+//     if !visited[nextNode] {
+//       visited[nextNode] = true
+//       queue.append(nextNode)
+//     }
+//   }
+//   index += 1
+// }
+
+// print(visited.filter { $0 }.count - 1)
+
+/// DFS
+let n = Int(readLine()!)!
+let m = Int(readLine()!)!
+var graph: [[Int]] = Array(repeating: [], count: n + 1)
+var visited: [Bool] = Array(repeating: false, count: n + 1)
+
+for _ in 0..<m {
+  let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
+  let a = input[0]
+  let b = input[1]
+  graph[a].append(b)
+  graph[b].append(a)
 }
 
-var queue = [1]
-var index = 0
-visited[1] = true
+func dfs(node: Int) {
+  visited[node] = true
 
-while queue.count > index {
-  let currendNode = queue[index]
-  for nextNode in graph[currendNode] {
-    if !visited[nextNode] {
-      visited[nextNode] = true
-      queue.append(nextNode)
-    }
+  for nextNode in graph[node] where !visited[nextNode] {
+    dfs(node: nextNode)
   }
-  index += 1
 }
 
+dfs(node: 1)
 print(visited.filter { $0 }.count - 1)
